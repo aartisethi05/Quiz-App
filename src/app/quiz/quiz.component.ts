cbbbbcda5cfff01224ade9d12f740e5563020081
aartisethi <aartisethi8194@gmail.com>
2020-06-26 19:41:35 +0530

    fix: quiz timer and auto save fixed

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,62 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { FormGroup ,FormBuilder,FormControl} from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  styleUrls: ['./quiz.component.css'],
 })
 export class QuizComponent implements OnInit {
-  quizForm:FormGroup;
-  constructor(private router:Router,private fb:FormBuilder) {
-    this.quizForm=fb.group({
-      ques:[]
-    })
-   }
-  
-  ngOnInit(): void {
-    
-  }
-  ngAfterViewInit(): void{
-    this.clock();
-    
-  }
-  
-  time:any="10:00";
-  questions=['aaa','bbb'];
+  quizForm: FormGroup;
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.quizForm = fb.group({ ques: [] });
 
-  getResult(){
-    this.router.navigate(["result"]);
-    console.log(this.quizForm.value);
+    this.timer = null;
+
+    this.time = null;
   }
-   startTimer(duration) {
-    var timer = duration;
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    this.clock();
+  }
+  time: any = '10:00';
+  questions = ['aaa', 'bbb'];
+  timer: null | NodeJS.Timeout;
+  getResult() {
+    this.router.navigate(['result']);
+    console.log(this.quizForm.value);
+    clearInterval(this.timer);
+  }
+  startTimer(durationInSeconds) {
     var minutes;
     var seconds;
-    var clock=setInterval(function () {
-        minutes = timer / 60;
-        seconds = timer % 60;
-        minutes=parseInt(minutes,10);
-        seconds=parseInt(seconds,10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-  
-        this.time = minutes + ":" + seconds;
-        console.log(this.time);
-        if (--timer < 0) {
-          alert('timed out');
-          clearInterval(clock);
-          
-        }
+    this.timer = setInterval(() => {
+      minutes = durationInSeconds / 60;
+      seconds = durationInSeconds % 60;
+      minutes = parseInt(minutes, 10);
+      seconds = parseInt(seconds, 10);
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+      this.time = minutes + ':' + seconds;
+      durationInSeconds--;
+      console.log(durationInSeconds);
+      if (durationInSeconds === 0) {
+        alert('timeout');
+        this.getResult();
+      }
     }, 1000);
-    
-}
-
- clock() {
-    var tenMinutes = 60 * 1;
-    this.startTimer(tenMinutes);
-    
-};
-
-
+  }
+  clock() {
+    var timeInSeconds = 60;
+    this.startTimer(timeInSeconds);
+    setTimeout(this.getResult, timeInSeconds * 1000);
+  }
 }
